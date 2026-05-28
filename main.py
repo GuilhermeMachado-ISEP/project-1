12bd800036737fa5c061ac6d3b0469205f3f53bb
Guilherme Machado-ISEP <1240713@isep.ipp.pt>
2026-05-28 14:16:23 +0100

    Modifying the program in order to be able to only encrypt, decrypt, or to do one then the other

--- main.py
+++ main.py
@@ -2,14 +2,37 @@ from encrypt import encrypt
 from decrypt import decrypt
 
 def main():
-    print('Welcome to the CypherCLI main interface, please input a String to be encrypted')
+    print('Welcome to the CypherCLI main interface, please make a choice between one of the options below')
 
-    s = encrypt(input('Enter a String: '))
+    print('1: Just Encrypt')
 
-    print('Encrypted String: ', s)
+    print('2: Just Decrypt')
 
-    s = decrypt(s)
+    print('3: Encrypt, then Decrypt')
 
-    print('Decrypted String: ', s)
+    print('0: Exit')
+
+    s = int(input('Enter a option: '))
+
+    while 0 > s > 3:
+        print('Invalid option')
+        s = int(input('Enter a option: '))
+
+    if s == 1:
+        s = encrypt(input('Enter a String: '))
+        print('Encrypted String: "', s, '"')
+    elif s == 2:
+        s = input('Enter a String: ')
+        if (int(s[-2]) != 0 and int(s[-2]) != 1) or (int(s[-1]) != 0 and int(s[-1]) != 1):
+            print('String was not encrypted by this cypher')
+        else:
+            s = decrypt(s)
+            print(f'Decrypted String: "{s}"')
+    elif s == 3:
+        s = encrypt(input('Enter a String: '))
+        print('Encrypted String: ', s)
+        s = decrypt(s)
+        print('Decrypted String: ', s)
+
 
 main()
